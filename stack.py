7c45056f8adf12e1e789b46610693f1a43d9a7f7
Fizakhan10258 <135802838+Fizakhan10258@users.noreply.github.com>
2023-06-06 23:26:15 -0700

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,26 @@
+### implement stack using array
+
+import array as myarray
+
+my_stack=myarray.array("i",[1,2,3,4,5,6,7,8,9,10])
+
+### reading the Aarray/stack
+
+for i in my_stack:
+    print(i,end=" ")
+print("\n")    
+
+### inserting element in the stack
+
+my_stack.append(11)
+for i in my_stack:
+    print(i,end=" ")
+print("\n")    
+
+### deleting element from the stack
+
+for i in range(0,len(my_stack)):
+    my_stack.pop()
+    for i in my_stack:
+        print(i,end=" ")
+    print("\n")    
